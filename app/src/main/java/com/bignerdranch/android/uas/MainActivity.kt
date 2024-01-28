c3154e8c0963f6974624da7a7d898022d53e2208
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 20:56:44 +0700

    MainActivity: Starting CheatActivity

--- app/src/main/java/com/bignerdranch/android/uas/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/uas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.uas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -72,6 +73,8 @@ class MainActivity : AppCompatActivity() {
 
             binding.cheatButton.setOnClickListener {
                 // Start CheatActivity
+                val intent = Intent(this, CheatActivity::class.java)
+                startActivity(intent)
             }
 
         }
