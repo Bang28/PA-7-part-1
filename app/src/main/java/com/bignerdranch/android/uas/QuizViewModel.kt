5e5be44c9fcf9088be7f975611bf93f36b9cb092
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 20:03:21 +0700

    QuizViewModel: Pasting model data into QuizViewModel

--- app/src/main/java/com/bignerdranch/android/uas/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/uas/QuizViewModel.kt
@@ -12,4 +12,14 @@ class QuizViewModel : ViewModel() {
         super.onCleared()
         Log.d(TAG, "ViewModel instance about to be destroyed")
     }
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 }
