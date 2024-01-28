69619d3af3762d378a06dcc5be99de1f0eb852a6
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 19:47:39 +0700

    MainActivity: Overriding more lifecycle functions

--- app/src/main/java/com/bignerdranch/android/uas/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/uas/MainActivity.kt
@@ -72,6 +72,28 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
