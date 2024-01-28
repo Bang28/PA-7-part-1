f76780f6eddd3706551b0dd4eec7bde93cc23673
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 20:53:56 +0700

    CheatActivity: Using View Binding in CheatActivity

--- app/src/main/java/com/bignerdranch/android/uas/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/uas/CheatActivity.kt
@@ -2,10 +2,16 @@ package com.bignerdranch.android.uas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.uas.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
